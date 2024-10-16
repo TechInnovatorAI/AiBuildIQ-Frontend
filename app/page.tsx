0255c882568b4b3ffa6f76849a8f0a7638af1009
JohnLiver <tipgreen47@gmail.com>
2024-10-16 11:28:27 -0700

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Common from "@/components/landing/Common";
-import Features from "@/components/landing/Features";
 import Hero from "@/components/landing/home/Hero";
 import Hero2 from "@/components/landing/home/Hero2";
 import Innovation from "@/components/landing/home/Innovation";
@@ -9,14 +7,6 @@ import Product from "@/components/landing/home/Product";
 import Product2 from "@/components/landing/home/Product2";
 import Product3 from "@/components/landing/home/Product3";
 import Product4 from "@/components/landing/home/Product4";
-import FeaturesImage from "@/public/assets/features.svg";
-import Bell from "@/public/assets/bell.svg";
-import Cstar from "@/public/assets/cStar.svg";
-import Vise from "@/public/assets/visa.svg";
-import Testimonials from "@/components/landing/Testimonial";
-import Faq from "@/components/landing/Faq";
-import Started from "@/components/landing/Started";
-import Star from "@/public/assets/start.svg";
 
 export default function Home() {
   return (
